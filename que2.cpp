580d8cba778a1f7ef8f9582e61a5e2c729bddf6b
khwaishgrg7 <khwaishgrg7@gmail.com>
2026-01-21 13:59:12 +0530

    Add files via upload

---
+++ que2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>  
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter the value of r=";
+    cin>>r;
+    float area =(3.14*r*r);
+    cout<<"\n Enter the value the area ="<<area;
+    return 0;
+}
