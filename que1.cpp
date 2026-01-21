79fcec372d66729c20e1602125699dfa46e2dc77
khwaishgrg7 <khwaishgrg7@gmail.com>
2026-01-21 13:58:33 +0530

    Add files via upload

---
+++ que1.cpp
@@ -0,0 +1,12 @@
+
+
+#include<iostream>  
+using namespace std;
+int main(){
+    float a,b,c;
+    cout<<"Enter the value of a,b,c:";
+    cin>>a>>b>>c;
+     float avg=(a+b+c)/3;
+cout<<"\nEnter the vlaue of avg="<<avg;
+return 0;
+}
