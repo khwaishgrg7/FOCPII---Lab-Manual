fd2234ebe448087b4de70acabfc42204e16608dc
khwaishgrg7 <khwaishgrg7@gmail.com>
2026-02-02 23:44:38 +0530

    Add files via upload

---
+++ que19.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+cout<<"Enter two num:";
+cin>>a>>b;
+
+ cout<<"prime no are :";
+
+for(int i=a;i<=b;i++){
+    if(i=2){
+        continue;
+    }
+
+}
+
