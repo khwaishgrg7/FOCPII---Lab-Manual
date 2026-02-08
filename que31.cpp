5c02481081d251b290b7d1273950583529dc6c86
khwaishgrg7 <khwaishgrg7@gmail.com>
2026-02-08 13:50:58 +0530

    Add files via upload

---
+++ que31.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[5], count = 0;
+
+    cout << "Enter 5 numbers:\n";
+    for (int i = 0; i < 5; i++) {
+        cin >> a[i];
+        if (a[i] % 3 == 0 && a[i] % 5 == 0) {
+            count++;
+        }
+    }
+
+    cout << "Count of numbers divisible by 3 and 5 = " << count << endl;
+
+    return 0;
+}
